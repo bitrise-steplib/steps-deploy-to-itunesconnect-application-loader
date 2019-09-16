59e7786b4066dc0ff69d45fab5f6f2f0bce11227
Richard Bogdan <richard.bogdan@bitrise.io>
2019-09-16 16:14:05 +0200

    TOOL-1134 Added APIKey and Issuer ID as input to enable JWT authentication for uploading.

--- main.go
+++ main.go
@@ -94,9 +94,9 @@ func getUploadCommand(xcodeVersion models.XcodebuildVersionModel, cfg Config, xc
 	var cmd *command.Model
 	if xcodeVersion.MajorVersion < 11 {
 		altool := filepath.Join(xcpath, "/Contents/Applications/Application Loader.app/Contents/Frameworks/ITunesSoftwareService.framework/Support/altool")
-		cmd = command.New(altool, "--upload-app", "-f", filePth, authOpts)
+		cmd = command.New(altool, append([]string{"--upload-app", "-f", filePth}, authOpts...)...)
 	} else {
-		cmd = command.New("xcrun", "altool", "--upload-app", "-f", authOpts)
+		cmd = command.New("xcrun", append([]string{"altool", "--upload-app", "-f"}, authOpts...)...)
 	}
 	cmd.SetStdout(os.Stdout)
 	cmd.SetStderr(os.Stderr)
@@ -113,12 +113,12 @@ func getUploadCommand(xcodeVersion models.XcodebuildVersionModel, cfg Config, xc
 
 // getAuthOptions provides the command options for authentication. Either a user and password pair or API key and issuer
 // ID should be set in the config.
-func getAuthOptions(cfg Config) (string, string, error) {
+func getAuthOptions(cfg Config) ([]string, string, error) {
 	passOptions, redact, passErr := getUserAndPasswordOptions(cfg)
 	if passErr != nil {
 		apiOptions, apiErr := getAPIKeyAndIssuerOptions(cfg)
 		if apiErr != nil {
-			return "", "", fmt.Errorf("neither usen and password or API key and issuer ID is provided correctly, please define one of the pairs in the config.\nIssue with user and password: %s\nIssue with API key and issuer ID: %s", passErr, apiErr)
+			return []string{}, "", fmt.Errorf("neither usen and password or API key and issuer ID is provided correctly, please define one of the pairs in the config.\nIssue with user and password: %s\nIssue with API key and issuer ID: %s", passErr, apiErr)
 		}
 		return apiOptions, "", nil
 	}
@@ -126,26 +126,26 @@ func getAuthOptions(cfg Config) (string, string, error) {
 }
 
 // getUserAndPasswordOptions provides the user and password pair for authentication.
-func getUserAndPasswordOptions(cfg Config) (string, string, error) {
+func getUserAndPasswordOptions(cfg Config) ([]string, string, error) {
 	if cfg.ItunesConnectUser == "" {
-		return "", "", fmt.Errorf("iTunes Connect user is not configured, please define it in the config")
+		return []string{}, "", fmt.Errorf("iTunes Connect user is not configured, please define it in the config")
 	}
 	password := getPasswordFromConfig(cfg)
 	if password == "" {
-		return "", "", fmt.Errorf("neither password or app password is configured, please define it the config")
+		return []string{}, "", fmt.Errorf("neither password or app password is configured, please define it the config")
 	}
-	return fmt.Sprintf("-u %s -p %s", cfg.ItunesConnectUser, password), password, nil
+	return []string{"-u", cfg.ItunesConnectUser, "-p", password}, password, nil
 }
 
 // getAPIKeyAndIsssuerOptions provides the API key and issuer ID pair for authentication.
-func getAPIKeyAndIssuerOptions(cfg Config) (string, error) {
+func getAPIKeyAndIssuerOptions(cfg Config) ([]string, error) {
 	if cfg.APIKey == "" {
-		return "", fmt.Errorf("API key is not configured, please define it in the config")
+		return []string{}, fmt.Errorf("API key is not configured, please define it in the config")
 	}
 	if cfg.APIIssuer == "" {
-		return "", fmt.Errorf("API Issuer is not configured, please define it in the config")
+		return []string{}, fmt.Errorf("API Issuer is not configured, please define it in the config")
 	}
-	return fmt.Sprintf("--apiKey %s --apiIssuer %s", cfg.APIKey, cfg.APIIssuer), nil
+	return []string{"--apiKey", cfg.APIKey, "--apiIssuer", cfg.APIIssuer}, nil
 }
 
 func getPasswordFromConfig(cfg Config) string {
--- main_test.go
+++ main_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"reflect"
 	"testing"
 
 	"github.com/bitrise-io/go-utils/sliceutil"
@@ -20,20 +21,20 @@ func Test_getAuthOptions(t *testing.T) {
 	tests := []struct {
 		name    string
 		cfg     Config
-		want    string
+		want    []string
 		want1   string
 		wantErr bool
 	}{
-		{"userAndPassword", Config{"dummyIPAPath", "dummyPkgPath", "iTunesUser", "password", "", "", ""}, "-u iTunesUser -p password", "password", false},
-		{"userAndAppPassword1", Config{"dummyIPAPath", "dummyPkgPath", "iTunesUser", "", "appPassword", "", ""}, "-u iTunesUser -p appPassword", "appPassword", false},
-		{"userAndAppPassword2", Config{"dummyIPAPath", "dummyPkgPath", "iTunesUser", "password", "appPassword", "", ""}, "-u iTunesUser -p appPassword", "appPassword", false},
-		{"apiKeyAndIssuerID", Config{"dummyIPAPath", "dummyPkgPath", "", "", "", "APIKey", "IssuerID"}, "--apiKey APIKey --apiIssuer IssuerID", "", false},
-		{"allProvided", Config{"dummyIPAPath", "dummyPkgPath", "iTunesUser", "password", "appPassword", "APIKey", "IssuerID"}, "-u iTunesUser -p appPassword", "appPassword", false},
-		{"missingPassword", Config{"dummyIPAPath", "dummyPkgPath", "iTunesUser", "", "", "", ""}, "", "", true},
-		{"missingUser", Config{"dummyIPAPath", "dummyPkgPath", "", "password", "appPassword", "", ""}, "", "", true},
-		{"missingAPIKey", Config{"dummyIPAPath", "dummyPkgPath", "", "", "", "", "IssuerID"}, "", "", true},
-		{"missingAPIIssuer", Config{"dummyIPAPath", "dummyPkgPath", "", "", "", "APIKey", ""}, "", "", true},
-		{"allMissing", Config{"dummyIPAPath", "dummyPkgPath", "", "", "", "", ""}, "", "", true},
+		{"userAndPassword", Config{"dummyIPAPath", "dummyPkgPath", "iTunesUser", "password", "", "", ""}, []string{"-u", "iTunesUser", "-p", "password"}, "password", false},
+		{"userAndAppPassword1", Config{"dummyIPAPath", "dummyPkgPath", "iTunesUser", "", "appPassword", "", ""}, []string{"-u", "iTunesUser", "-p", "appPassword"}, "appPassword", false},
+		{"userAndAppPassword2", Config{"dummyIPAPath", "dummyPkgPath", "iTunesUser", "password", "appPassword", "", ""}, []string{"-u", "iTunesUser", "-p", "appPassword"}, "appPassword", false},
+		{"apiKeyAndIssuerID", Config{"dummyIPAPath", "dummyPkgPath", "", "", "", "APIKey", "IssuerID"}, []string{"--apiKey", "APIKey", "--apiIssuer", "IssuerID"}, "", false},
+		{"allProvided", Config{"dummyIPAPath", "dummyPkgPath", "iTunesUser", "password", "appPassword", "APIKey", "IssuerID"}, []string{"-u", "iTunesUser", "-p", "appPassword"}, "appPassword", false},
+		{"missingPassword", Config{"dummyIPAPath", "dummyPkgPath", "iTunesUser", "", "", "", ""}, []string{}, "", true},
+		{"missingUser", Config{"dummyIPAPath", "dummyPkgPath", "", "password", "appPassword", "", ""}, []string{}, "", true},
+		{"missingAPIKey", Config{"dummyIPAPath", "dummyPkgPath", "", "", "", "", "IssuerID"}, []string{}, "", true},
+		{"missingAPIIssuer", Config{"dummyIPAPath", "dummyPkgPath", "", "", "", "APIKey", ""}, []string{}, "", true},
+		{"allMissing", Config{"dummyIPAPath", "dummyPkgPath", "", "", "", "", ""}, []string{}, "", true},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
@@ -42,7 +43,7 @@ func Test_getAuthOptions(t *testing.T) {
 				t.Errorf("getAuthOptions() error = %v, wantErr %v", err, tt.wantErr)
 				return
 			}
-			if got != tt.want {
+			if !reflect.DeepEqual(got, tt.want) {
 				t.Errorf("getAuthOptions() got = %v, want %v", got, tt.want)
 			}
 			if got1 != tt.want1 {
