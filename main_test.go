4603de81ef46e4a71d9658b0b028a6ffc764baeb
Richard Bogdan <richard.bogdan@bitrise.io>
2019-09-16 15:54:37 +0200

    TOOL-1134 Added APIKey and Issuer ID as input to enable JWT authentication for uploading.

--- main_test.go
+++ main_test.go
@@ -15,3 +15,39 @@ func Test_xcodePath(t *testing.T) {
 		require.True(t, sliceutil.IsStringInSlice(got, []string{"/Applications/Xcode.app", "/Applications/Xcode-beta.app"}))
 	}
 }
+
+func Test_getAuthOptions(t *testing.T) {
+	tests := []struct {
+		name    string
+		cfg     Config
+		want    string
+		want1   string
+		wantErr bool
+	}{
+		{"userAndPassword", Config{"dummyIPAPath", "dummyPkgPath", "iTunesUser", "password", "", "", ""}, "-u iTunesUser -p password", "password", false},
+		{"userAndAppPassword1", Config{"dummyIPAPath", "dummyPkgPath", "iTunesUser", "", "appPassword", "", ""}, "-u iTunesUser -p appPassword", "appPassword", false},
+		{"userAndAppPassword2", Config{"dummyIPAPath", "dummyPkgPath", "iTunesUser", "password", "appPassword", "", ""}, "-u iTunesUser -p appPassword", "appPassword", false},
+		{"apiKeyAndIssuerID", Config{"dummyIPAPath", "dummyPkgPath", "", "", "", "APIKey", "IssuerID"}, "--apiKey APIKey --apiIssuer IssuerID", "", false},
+		{"allProvided", Config{"dummyIPAPath", "dummyPkgPath", "iTunesUser", "password", "appPassword", "APIKey", "IssuerID"}, "-u iTunesUser -p appPassword", "appPassword", false},
+		{"missingPassword", Config{"dummyIPAPath", "dummyPkgPath", "iTunesUser", "", "", "", ""}, "", "", true},
+		{"missingUser", Config{"dummyIPAPath", "dummyPkgPath", "", "password", "appPassword", "", ""}, "", "", true},
+		{"missingAPIKey", Config{"dummyIPAPath", "dummyPkgPath", "", "", "", "", "IssuerID"}, "", "", true},
+		{"missingAPIIssuer", Config{"dummyIPAPath", "dummyPkgPath", "", "", "", "APIKey", ""}, "", "", true},
+		{"allMissing", Config{"dummyIPAPath", "dummyPkgPath", "", "", "", "", ""}, "", "", true},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, got1, err := getAuthOptions(tt.cfg)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("getAuthOptions() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if got != tt.want {
+				t.Errorf("getAuthOptions() got = %v, want %v", got, tt.want)
+			}
+			if got1 != tt.want1 {
+				t.Errorf("getAuthOptions() got1 = %v, want %v", got1, tt.want1)
+			}
+		})
+	}
+}
