d172dc4a0d41897a8febb430e376dca263315e16
Krisztián Gödrei <krisztian.godrei@bitrise.io>
2021-02-04 15:48:10 +0100

    Step 388 connection (#27)
    * Add new connection input.
    
    * Add connection input to Config model.
    
    * Wire in connection, drop Xcode < 11 support.
    
    * Fix redaction.
    
    * Gitignore api keys.
    
    * Update bitrise.yml.
    
    * Update test project clone step.
    
    * Fix build number update.
    
    * Turn on connection tests.
    
    * Fix wf name.
    
    * Fix input validation.
    
    * Update logs.
    
    * Fix authentication params.
    
    * Fix missing app-specific password.
    
    * Update step.yml
    
    * Debug app specific password.
    
    * Fix step.yml wording.
    
    * Update step.yml and connection logging.

--- main.go
+++ main.go
@@ -3,129 +3,100 @@ package main
 import (
 	"bytes"
 	"fmt"
-	"io"
-	"io/ioutil"
 	"net/http"
-	"net/url"
 	"os"
 	"path/filepath"
 	"regexp"
 	"strings"
 
+	"github.com/bitrise-io/go-utils/fileutil"
 	"github.com/bitrise-io/go-utils/pathutil"
+	"github.com/bitrise-steplib/steps-deploy-to-itunesconnect-deliver/appleauth"
+	"github.com/bitrise-steplib/steps-deploy-to-itunesconnect-deliver/devportalservice"
 
 	"github.com/bitrise-io/go-utils/command"
 	"github.com/bitrise-io/go-utils/log"
-	"github.com/bitrise-io/go-xcode/utility"
-	"github.com/bitrise-tools/go-steputils/input"
 	"github.com/bitrise-tools/go-steputils/stepconf"
 	shellquote "github.com/kballard/go-shellquote"
 )
 
 // Config ...
 type Config struct {
-	IpaPath           string          `env:"ipa_path"`
-	PkgPath           string          `env:"pkg_path"`
-	ItunesConnectUser string          `env:"itunescon_user"`
-	Password          stepconf.Secret `env:"password"`
-	AppPassword       stepconf.Secret `env:"app_password"`
-	APIKeyPath        string          `env:"api_key_path"`
-	APIIssuer         string          `env:"api_issuer"`
-	AdditionalParams  string          `env:"altool_options"`
+	BitriseConnection   string          `env:"connection,opt[automatic,api_key,apple_id,off]"`
+	AppleID             string          `env:"itunescon_user"`
+	Password            stepconf.Secret `env:"password"`
+	AppSpecificPassword stepconf.Secret `env:"app_password"`
+	APIKeyPath          stepconf.Secret `env:"api_key_path"`
+	APIIssuer           string          `env:"api_issuer"`
+
+	IpaPath           string `env:"ipa_path"`
+	PkgPath           string `env:"pkg_path"`
+	ItunesConnectUser string `env:"itunescon_user"`
+	AdditionalParams  string `env:"altool_options"`
+
+	// Used to get Bitrise Apple Developer Portal Connection
+	BuildURL      string          `env:"BITRISE_BUILD_URL"`
+	BuildAPIToken stepconf.Secret `env:"BITRISE_BUILD_API_TOKEN"`
 }
 
-func (cfg Config) validateEnvs() error {
-	if err := input.ValidateIfNotEmpty(cfg.IpaPath); err != nil {
-		if err := input.ValidateIfNotEmpty(cfg.PkgPath); err != nil {
-			return fmt.Errorf("neither ipa_path nor pkg_path is provided")
-		}
-	}
+func (cfg Config) validateArtifact() error {
+	cfg.IpaPath = strings.TrimSpace(cfg.IpaPath)
+	cfg.PkgPath = strings.TrimSpace(cfg.PkgPath)
 
 	var (
-		isJWTAuthType     = (cfg.APIKeyPath != "" || cfg.APIIssuer != "")
-		isAppleIDAuthType = (cfg.AppPassword != "" || cfg.Password != "" || cfg.ItunesConnectUser != "")
+		deployIPA = cfg.IpaPath != ""
+		deployPKG = cfg.PkgPath != ""
 	)
 
-	switch {
-
-	case isAppleIDAuthType == isJWTAuthType:
-
-		return fmt.Errorf("one type of authentication required, either provide itunescon_user with password/app_password or api_key_path with api_issuer")
-
-	case isAppleIDAuthType:
-
-		if err := input.ValidateIfNotEmpty(string(cfg.ItunesConnectUser)); err != nil {
-			return fmt.Errorf("no itunescon_user provided")
-		}
-		if err := input.ValidateIfNotEmpty(string(cfg.Password)); err != nil {
-			if err := input.ValidateIfNotEmpty(string(cfg.AppPassword)); err != nil {
-				return fmt.Errorf("neither password nor app_password is provided")
-			}
-		}
-
-	case isJWTAuthType:
-
-		if err := input.ValidateIfNotEmpty(string(cfg.APIIssuer)); err != nil {
-			return fmt.Errorf("no api_issuer provided")
-		}
-		if err := input.ValidateIfNotEmpty(string(cfg.APIKeyPath)); err != nil {
-			return fmt.Errorf("no api_key_path provided")
-		}
-
+	if deployIPA == deployPKG {
+		return fmt.Errorf("one artifact is required and only one is allowed, either provide ipa_path or pkg_path")
 	}
 
 	return nil
 }
 
-func copyOrDownloadFile(u *url.URL, pth string) error {
-	if err := os.MkdirAll(filepath.Dir(pth), 0777); err != nil {
-		return err
+func parseAuthSources(connection string) ([]appleauth.Source, error) {
+	switch connection {
+	case "automatic":
+		return []appleauth.Source{
+			&appleauth.ConnectionAPIKeySource{},
+			&appleauth.ConnectionAppleIDSource{},
+			&appleauth.InputAPIKeySource{},
+			&appleauth.InputAppleIDSource{},
+		}, nil
+	case "api_key":
+		return []appleauth.Source{&appleauth.ConnectionAPIKeySource{}}, nil
+	case "apple_id":
+		return []appleauth.Source{&appleauth.ConnectionAppleIDSource{}}, nil
+	case "off":
+		return []appleauth.Source{
+			&appleauth.InputAPIKeySource{},
+			&appleauth.InputAppleIDSource{},
+		}, nil
+	default:
+		return nil, fmt.Errorf("invalid connection input: %s", connection)
 	}
-
-	certFile, err := os.Create(pth)
-	if err != nil {
-		return err
-	}
-	defer func() {
-		if err := certFile.Close(); err != nil {
-			log.Errorf("Failed to close file, error: %s", err)
-		}
-	}()
-
-	// if file -> copy
-	if u.Scheme == "file" {
-		b, err := ioutil.ReadFile(u.Path)
-		if err != nil {
-			return err
-		}
-		_, err = certFile.Write(b)
-		return err
-	}
-
-	// otherwise download
-	f, err := http.Get(u.String())
-	if err != nil {
-		return err
-	}
-	defer func() {
-		if err := f.Body.Close(); err != nil {
-			log.Errorf("Failed to close file, error: %s", err)
-		}
-	}()
-
-	_, err = io.Copy(certFile, f.Body)
-	return err
 }
 
-func getKeyID(u *url.URL) string {
-	var keyID = "Bitrise" // as default if no ID found in file name
+const notConnected = `Connected Apple Developer Portal Account not found.
+Most likely because there is no Apple Developer Portal Account connected to the build.
+Read more: https://devcenter.bitrise.io/getting-started/configuring-bitrise-steps-that-require-apple-developer-account-data/`
 
-	// get the ID of the key from the file
-	if matches := regexp.MustCompile(`AuthKey_(.+)\.p8`).FindStringSubmatch(filepath.Base(u.Path)); len(matches) == 2 {
-		keyID = matches[1]
+func handleSessionDataError(err error) {
+	if err == nil {
+		return
 	}
 
-	return keyID
+	if networkErr, ok := err.(devportalservice.NetworkError); ok && networkErr.Status == http.StatusUnauthorized {
+		fmt.Println()
+		log.Warnf("%s", "Unauthorized to query Connected Apple Developer Portal Account. This happens by design, with a public app's PR build, to protect secrets.")
+
+		return
+	}
+
+	fmt.Println()
+	log.Errorf("Failed to activate Bitrise Apple Developer Portal connection: %s", err)
+	log.Warnf("Read more: https://devcenter.bitrise.io/getting-started/configuring-bitrise-steps-that-require-apple-developer-account-data/")
 }
 
 func getKeyPath(keyID string, keyPaths []string) (string, error) {
@@ -145,8 +116,7 @@ func getKeyPath(keyID string, keyPaths []string) (string, error) {
 	return filepath.Join(keyPaths[0], certName), nil
 }
 
-// prepares key and returns the ID the tool need to use
-func prepareAPIKey(apiKeyPath string) (string, error) {
+func writeAPIKey(privateKey, keyID string) error {
 	// see these in the altool's man page
 	var keyPaths = []string{
 		filepath.Join(os.Getenv("HOME"), ".appstoreconnect/private_keys"),
@@ -155,27 +125,19 @@ func prepareAPIKey(apiKeyPath string) (string, error) {
 		"./private_keys",
 	}
 
-	fileURL, err := url.Parse(apiKeyPath)
-	if err != nil {
-		return "", err
-	}
-
-	keyID := getKeyID(fileURL)
-
 	keyPath, err := getKeyPath(keyID, keyPaths)
 	if err != nil {
 		if err == os.ErrExist {
-			return keyID, nil
+			return nil
 		}
-		return "", err
+		return err
 	}
 
-	// cert file not found on any of the locations, so copy or download it then return it's ID
-	if err := copyOrDownloadFile(fileURL, keyPath); err != nil {
-		return "", err
+	if err := os.MkdirAll(filepath.Dir(keyPath), 0777); err != nil {
+		return err
 	}
 
-	return keyID, nil
+	return fileutil.WriteStringToFile(keyPath, privateKey)
 }
 
 func main() {
@@ -187,66 +149,101 @@ func main() {
 	stepconf.Print(cfg)
 	fmt.Println()
 
-	if err := cfg.validateEnvs(); err != nil {
+	if err := cfg.validateArtifact(); err != nil {
 		failf("Input error: %s", err)
 	}
 
-	password := string(cfg.Password)
-	if string(cfg.AppPassword) != "" {
-		password = string(cfg.AppPassword)
+	authInputs := appleauth.Inputs{
+		Username:            cfg.ItunesConnectUser,
+		Password:            string(cfg.Password),
+		AppSpecificPassword: string(cfg.AppSpecificPassword),
+		APIIssuer:           cfg.APIIssuer,
+		APIKeyPath:          string(cfg.APIKeyPath),
+	}
+	if err := authInputs.Validate(); err != nil {
+		failf("Issue with authentication related inputs: %v", err)
+	}
+
+	//
+	// Select and fetch Apple authenication source
+	authSources, err := parseAuthSources(cfg.BitriseConnection)
+	if err != nil {
+		failf("Invalid input: unexpected value for Bitrise Apple Developer Connection (%s)", cfg.BitriseConnection)
+	}
+
+	var devportalConnectionProvider *devportalservice.BitriseClient
+	if cfg.BuildURL != "" && cfg.BuildAPIToken != "" {
+		devportalConnectionProvider = devportalservice.NewBitriseClient(http.DefaultClient, cfg.BuildURL, string(cfg.BuildAPIToken))
+	} else {
+		fmt.Println()
+		log.Warnf("Connected Apple Developer Portal Account not found. Step is not running on bitrise.io: BITRISE_BUILD_URL and BITRISE_BUILD_API_TOKEN envs are not set")
+	}
+	var conn *devportalservice.AppleDeveloperConnection
+	if cfg.BitriseConnection != "off" && devportalConnectionProvider != nil {
+		var err error
+		conn, err = devportalConnectionProvider.GetAppleDeveloperConnection()
+		if err != nil {
+			handleSessionDataError(err)
+		}
+
+		if conn != nil && (conn.APIKeyConnection == nil && conn.AppleIDConnection == nil) {
+			fmt.Println()
+			log.Debugf("%s", notConnected)
+		}
+	}
+
+	authConfig, err := appleauth.Select(conn, authSources, authInputs)
+	if err != nil {
+		failf("Could not configure Apple Service authentication: %v", err)
+	}
+	if authConfig.AppleID != nil && authConfig.AppleID.AppSpecificPassword == "" {
+		log.Warnf("If 2FA enabled, Application-specific password is required when using Apple ID authentication.")
+	}
+
+	// Prepare command
+	var authParams []string
+	if authConfig.APIKey != nil {
+		if err := writeAPIKey(string(authConfig.APIKey.PrivateKey), authConfig.APIKey.KeyID); err != nil {
+			failf("Failed to prepare certificate for authentication, error: %s", err)
+		}
+		authParams = []string{"--apiKey", authConfig.APIKey.KeyID, "--apiIssuer", authConfig.APIKey.IssuerID}
+	} else {
+		password := string(authConfig.AppleID.Password)
+		if string(authConfig.AppleID.AppSpecificPassword) != "" {
+			password = string(authConfig.AppleID.AppSpecificPassword)
+		}
+		authParams = []string{"--username", authConfig.AppleID.Username, "--password", password}
 	}
 
 	filePth := cfg.IpaPath
-	if cfg.PkgPath != "" {
+	if filePth == "" {
 		filePth = cfg.PkgPath
 	}
-
-	xcpath, err := xcodePath()
-	if err != nil {
-		failf("Failed to find Xcode path, error: %s", err)
-	}
-
-	log.Printf("Xcode path: %s", xcpath)
-	fmt.Println()
-
-	xcodeVersion, err := utility.GetXcodeVersion()
-	if err != nil {
-		failf("Failed to determine Xcode version, error: %s", err)
-	}
-
-	authParams := []string{"-u", cfg.ItunesConnectUser, "-p", password}
 
 	additionalParams, err := shellquote.Split(cfg.AdditionalParams)
 	if err != nil {
 		failf("Failed to parse additional parameters, error: %s", err)
 	}
 
-	var cmd *command.Model
-	if xcodeVersion.MajorVersion < 11 {
-		altool := filepath.Join(xcpath, "/Contents/Applications/Application Loader.app/Contents/Frameworks/ITunesSoftwareService.framework/Support/altool")
-		cmd = command.New(altool, append(append([]string{"--upload-app", "-f", filePth}, authParams...), additionalParams...)...)
-	} else {
-		if cfg.APIKeyPath != "" {
-			apiKeyID, err := prepareAPIKey(cfg.APIKeyPath)
-			if err != nil {
-				failf("Failed to prepare certificate for authentication, error: %s", err)
-			}
-			authParams = []string{"--apiKey", apiKeyID, "--apiIssuer", cfg.APIIssuer}
-		}
-		cmd = command.New("xcrun", append(append([]string{"altool", "--upload-app", "-f", filePth}, authParams...), additionalParams...)...)
-	}
+	altoolParams := append([]string{"altool", "--upload-app", "-f", filePth}, authParams...)
+	altoolParams = append(altoolParams, additionalParams...)
+	cmd := command.New("xcrun", altoolParams...)
 	var outb bytes.Buffer
 	cmd.SetStdout(&outb)
 	cmd.SetStderr(os.Stderr)
 
 	fileName := filepath.Base(filePth)
-	commandStr := cmd.PrintableCommandArgs()
-
-	if len(password) > 0 {
-		commandStr = strings.Replace(commandStr, password, "[REDACTED]", -1)
-	}
-
 	log.Infof("Uploading - %s ...", fileName)
+
+	commandStr := cmd.PrintableCommandArgs()
+	if authConfig.APIKey == nil {
+		if authConfig.AppleID.Password != "" {
+			commandStr = strings.Replace(commandStr, authConfig.AppleID.Password, "[REDACTED]", -1)
+		}
+		if authConfig.AppleID.AppSpecificPassword != "" {
+			commandStr = strings.Replace(commandStr, authConfig.AppleID.AppSpecificPassword, "[REDACTED]", -1)
+		}
+	}
 	log.Printf("$ %s", commandStr)
 
 	if err := cmd.Run(); err != nil {
@@ -262,27 +259,6 @@ func main() {
 	fmt.Println(out)
 
 	log.Donef("IPA uploaded")
-}
-
-func xcodePath() (string, error) {
-	cmd := command.New("xcode-select", "-p")
-
-	log.Infof("Get Xcode path")
-	log.Printf(cmd.PrintableCommandArgs())
-
-	resp, err := cmd.RunAndReturnTrimmedOutput()
-	if err != nil {
-		return "", err
-	}
-
-	// Default: /Applications/Xcode.app/Contents/Developer
-	// Beta: /Applications/Xcode-beta.app/Contents/Developer
-	split := strings.Split(resp, "/Contents")
-	if len(split) != 2 {
-		return "", fmt.Errorf("failed to find Xcode path")
-	}
-
-	return split[0], nil
 }
 
 func failf(format string, v ...interface{}) {
