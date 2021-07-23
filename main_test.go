bd30a60dd3ae5b697cc368e2f94d016f166ef137
Mate Herber <mate.herber@bitrise.io>
2021-07-23 16:31:34 +0200

    Implement retry logic on errors that are expected to be recoverable

--- main_test.go
+++ main_test.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"errors"
+	"github.com/avast/retry-go/v3"
+	"github.com/stretchr/testify/assert"
 	"io/ioutil"
 	"os"
 	"path/filepath"
@@ -8,6 +11,20 @@ import (
 
 	"github.com/bitrise-io/go-utils/pathutil"
 )
+
+const unableToDetermine = `*** Error: Error uploading '/Users/vagrant/deploy/MY-test-ios.ipa'.
+*** Error: Failed to upload package. Unable to determine the application using bundleId: io.bitrise.test. (-19201)`
+
+const transporterService = `*** Error: Error uploading '/Users/vagrant/deploy/io.bitrise.ipa'.
+*** Error: could not find the service with interface (com.apple.transporter.osgi.TransporterService) (-18000)
+*** Error: Resolver: Install error - com.apple.transporter.aspera-macos-x64 Exception's name: org.osgi.framework.BundleException, Exception's message: An error occurred trying to read the bundle (-18000)`
+
+const invalidResponse = `*** Error: Server returned an invalid MIME type: text/plain, body: Unauthenticated
+
+Request ID: MQ4DVCDRVZ4VS33A66KIKKP6SQ.0.0
+*** Error: Errors uploading '/Users/vagrant/deploy/bitrise.ipa': (
+    "Error Domain=NSCocoaErrorDomain Code=-1011 \"Authentication failed\" UserInfo={NSLocalizedDescription=Authentication failed, NSLocalizedFailureReason=Failed to authenticate for session: (\n    \"Error Domain=ITunesConnectionAuthenticationErrorDomain Code=-26000 \\\"The server returned an invalid response. This may indicate that a network proxy is interfering with communication, or that Apple servers are having issues. Please try your request again later.\\\" UserInfo={NSLocalizedRecoverySuggestion=The server returned an invalid response. This may indicate that a network proxy is interfering with communication, or that Apple servers are having issues. Please try your request again later., NSLocalizedDescription=The server returned an invalid response. This may indicate that a network proxy is interfering with communication, or that Apple servers are having issues. Please try your request again later., NSLocalizedFailureReason=App Store operation failed.}\"\n)}"
+)`
 
 func Test_getKeyPath(t *testing.T) {
 	tmpPath, err := pathutil.NormalizedOSTempDirPath("testing")
@@ -62,4 +79,124 @@ func Test_getKeyPath(t *testing.T) {
 			}
 		})
 	}
+}
+
+func Test_uploadWithRetry(t *testing.T) {
+	tests := []struct {
+		name     string
+		uploader uploader
+		want     string
+		wantErr  bool
+	}{
+		{name: "Upload Successful", uploader: createUploaderWithSuccess(), want: "success", wantErr: false},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := uploadWithRetry(tt.uploader)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("uploadWithRetry() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if got != tt.want {
+				t.Errorf("uploadWithRetry() got = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_uploadSuccessful(t *testing.T) {
+	uploader := createUploaderWithSuccess()
+
+	result, err := uploadWithRetry(uploader)
+
+	assert.NoError(t, err)
+	assert.Equal(t, result, "success")
+	uploader.AssertNumberOfCalls(t, "upload", 1)
+}
+
+func Test_uploadFailsWithUnknownError(t *testing.T) {
+	uploader := createUploaderWithUnknownError()
+
+	_, err := uploadWithRetry(uploader)
+
+	assert.Error(t, err)
+	uploader.AssertNumberOfCalls(t, "upload", 1)
+}
+
+func Test_uploadRetriesOnUnableToDetermine(t *testing.T) {
+	uploader := createUploaderWithUnableToDetermineError()
+
+	_, err := uploadWithRetry(uploader, retry.Delay(0))
+
+	assert.Error(t, err)
+	uploader.AssertNumberOfCalls(t, "upload", 10)
+}
+
+func Test_uploadRetriesOnTransporterService(t *testing.T) {
+	uploader := createUploaderWithTransporterService()
+
+	_, err := uploadWithRetry(uploader, retry.Delay(0))
+
+	assert.Error(t, err)
+	uploader.AssertNumberOfCalls(t, "upload", 10)
+}
+
+func Test_uploadRetriesOnInvalidResponse(t *testing.T) {
+	uploader := createUploaderWithInvalidResponse()
+
+	_, err := uploadWithRetry(uploader, retry.Delay(0))
+
+	assert.Error(t, err)
+	uploader.AssertNumberOfCalls(t, "upload", 10)
+}
+
+func Test_uploadRecoversAfterErrorOnValidResponse(t *testing.T) {
+	uploader := createUploaderWithFailingAndRecoveringResponse()
+
+	result, err := uploadWithRetry(uploader, retry.Delay(0))
+
+	assert.NoError(t, err)
+	assert.Equal(t, result, "success")
+	uploader.AssertNumberOfCalls(t, "upload", 4)
+}
+
+
+
+func createUploaderWithUnknownError() (uploader *mockUploader) {
+	uploader = new(mockUploader)
+	uploader.On("upload").Return("", "unknown-error", errors.New("test-error"))
+	return
+}
+
+func createUploaderWithUnableToDetermineError() (uploader *mockUploader) {
+	uploader = new(mockUploader)
+	uploader.On("upload").Return("", unableToDetermine, errors.New("test-error"))
+	return
+}
+
+func createUploaderWithTransporterService() (uploader *mockUploader) {
+	uploader = new(mockUploader)
+	uploader.On("upload").Return("", transporterService, errors.New("test-error"))
+	return
+}
+
+func createUploaderWithInvalidResponse() (uploader *mockUploader) {
+	uploader = new(mockUploader)
+	uploader.On("upload").Return("", invalidResponse, errors.New("test-error"))
+	return
+}
+
+func createUploaderWithFailingAndRecoveringResponse() (uploader *mockUploader) {
+	uploader = new(mockUploader)
+	uploader.On("upload").Return("", unableToDetermine, errors.New("test-error")).Once()
+	uploader.On("upload").Return("", transporterService, errors.New("test-error")).Once()
+	uploader.On("upload").Return("", invalidResponse, errors.New("test-error")).Once()
+	uploader.On("upload").Return("success", "", nil)
+	return
+}
+
+func createUploaderWithSuccess() (uploader *mockUploader) {
+	uploader = new(mockUploader)
+	uploader.On("upload").Return("success", "", nil)
+	return
 }
---
+++ vendor/github.com/avast/retry-go/v3/go.mod
@@ -0,0 +1,5 @@
+module github.com/avast/retry-go/v3
+
+go 1.13
+
+require github.com/stretchr/testify v1.7.0
